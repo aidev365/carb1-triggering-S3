eb79624f664e3a410f6b45d94e5f85a58473bba2
aidev365 <92700596+aidev365@users.noreply.github.com>
2022-04-04 22:39:04 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -14,7 +14,7 @@ s3 = boto3.client('s3')
 s3_resource = boto3.resource('s3')
 
 
-destination_bucketname = os.environ['recognition']
+destination_bucketname = os.environ['new_yolov5']
 
 
 def lambda_handler(event, context):
